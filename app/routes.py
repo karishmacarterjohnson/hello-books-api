6956e27560000859fe026afa6f04621b71730f1f
karishma <karishmacarterjohnson@gmail.com>
2021-10-21 07:24:02 -0700

    migrations setup & remove hello world

--- app/routes.py
+++ app/routes.py
@@ -1,22 +1,5 @@
 from flask import Blueprint, jsonify
 
-hello_world_bp = Blueprint("hello_world", __name__)
-   
-@hello_world_bp.route("/hello-world", methods = ["GET"])
-def hello_endpoint():
-    body = "Hello, World!"
-    return body
-
-@hello_world_bp.route("/hello/JSON", methods = ["GET"])
-def karishma_info():
-    karishma_body = {
-        "name": "Karishma Johnson",
-        "message": "What's up?",
-        "hobbies": ["swimming", "rowing", "throwing", "printmaking"]
-    }
-
-    return karishma_body
-
 '''
 class Book:
     def __init__(self, id, title, description):
